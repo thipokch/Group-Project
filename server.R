183285f99ddacf38c74cbd4324fdb945a9713723
Dylan Desmond <Dylan Desmond>
2016-11-30 17:58:23 -0800

    Started work on statistics charts and ui

--- server.R
+++ server.R
@@ -1,12 +1,13 @@
 #
-# This is the server logic of a Shiny web application. You can run the 
+# This is the server logic of a Shiny web application. You can run the
 # application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
-# 
+#
 #    http://shiny.rstudio.com/
 #
 
+<<<<<<< HEAD
 library(shiny)
 library(plotly)
 
@@ -21,3 +22,16 @@ shinyServer(function(input, output, session) {
   source("scripts/chart3.R",  local = TRUE)$value
   
 })
+=======
+# library(shiny)
+#
+#
+ # Define server logic required to draw a histogram
+ shinyServer(function(input, output, session) {
+   source("scripts/chart1.R",  local = TRUE)$value
+   source("scripts/chart2.R",  local = TRUE)$value
+   source("scripts/chart3.R",  local = TRUE)$value
+
+ })
+
+>>>>>>> Started work on statistics charts and ui
