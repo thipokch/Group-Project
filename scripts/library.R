5b2996f908b376cd6248fa6d59fa6c515dd4a0dd
Anna <amaglio@uw.edu>
2016-11-30 18:03:18 -0800

    fixed major selection for chart 1

--- scripts/library.R
+++ scripts/library.R
@@ -9,9 +9,10 @@ select.category <- function(df, out.variable, default.selected) {
                        selected = 'Agriculture & Natural Resources')
 }
 
+
 text.select.major <- function(df, out.variable, default.selected) {
   major_index_list <- structure(as.list.data.frame(df$Major_code),
-                                names = as.character(df$Major))
+                      names = as.character(df$Major))
   updateSelectizeInput(session, 
                        out.variable ,
                        choices = major_index_list, 
@@ -22,7 +23,7 @@ group.select.major <- function(df, default.category) {
   select.category(df,'Category.Selected', default.category)
    observe({
     df.filtered <- filter(df, Major_category %in% input$Category.Selected)
-    text.select.major(df,'Major.Selected', df.filtered$Major_code)
+    text.select.major(df.filtered,'Major.Selected', df.filtered$Major_code)
    })
 }
 
